390f3362007e38d897efecece0ff204223d6809a
yasser-dlewati <yasdle@outlook.com>
2024-01-14 00:49:10 +0800

    Added screen indicator to replace nav bar

--- js/custom.js
+++ js/custom.js
@@ -41,10 +41,55 @@ $('#scroll-down').click(function () {
     }, 500);
 })
 
+function renderScreenIndicator() {
+    var count = $('section').length
+    var content = ''
+    for (var i = 0; i < count; i++) {
+        content += '<li></li>'
+    }
+
+    $('.screen-indicator').html(content)
+}
+
+function setScreenIndicator(index) {
+    if (index === 0) {
+        $('.screen-indicator').html('<li class="active"></li><li></li><li></li><li></li>')
+    }
+    if (index === 1) {
+        $('.screen-indicator').html('<li></li><li class="active"></li><li></li><li></li>')
+    }
+    if (index === 2) {
+        $('.screen-indicator').html('<li></li><li></li><li class="active"></li><li></li>')
+    }
+    if (index === 3) {
+        $('.screen-indicator').html('<li></li><li></li><li></li><li class="active"></li>')
+    }
+}
+
 $(document).ready(function () {
     setTimeout(() => {
         window.scrollTo(0, 0);
         var $loader = $(".loader").removeClass("loader");
         $loader.remove();
     }, 800);
+
+    renderScreenIndicator()
+    setScreenIndicator(0)
+    $(window).scroll(function () {
+        var b = $(window).scrollTop();
+        var sections = $("section")
+        if (b > $(sections[0]).offset().top && b < $(sections[1]).offset().top) {
+            setScreenIndicator(0)
+        }
+        else if (b > $(sections[1]).offset().top && b < $(sections[2]).offset().top) {
+            setScreenIndicator(1)
+        }
+        else if (b > $(sections[2]).offset().top && b < $(sections[3]).offset().top) {
+            setScreenIndicator(2)
+        }
+        else if (b > $(sections[3]).offset().top) {
+            setScreenIndicator(3)
+        }
+        console.log(b);
+    })
 })
