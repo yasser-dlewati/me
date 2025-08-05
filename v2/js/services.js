bf8452e647001e2b972698239998b645390d36e6
yasser-dlewati <yasdle@outlook.com>
2025-08-05 23:17:58 +0800

    Fixed visual aspects for services section

--- v2/js/services.js
+++ v2/js/services.js
@@ -7,8 +7,14 @@ $('.service').click( function() {
         let isTargetDescriptionOpen = targetDescription.hasClass('show');
         if (isTargetDescriptionOpen) {
             $(this).find('i').removeClass('fa-square-plus').addClass('fa-square-minus');
+            //$(this).css('border-bottom', 'none');
+            $(this).css('border-bottom', 'none');
+            $('.service').not(this).css('border-bottom', '1px solid #ccc');
+            // targetDescription.css('border-bottom', '1px solid #ccc');
         } else {
             $(this).find('i').removeClass('fa-square-minus').addClass('fa-square-plus');
+             //$(this).css('border-bottom', 'none');
+            // targetDescription.css('border-bottom', 'none');
         }
     }, 400);
 });
