ec7a61d34ff0d519dd89479698aac0690de121a5
yasser-dlewati <yasdle@outlook.com>
2025-09-07 01:04:57 +0800

    Saved settings in local storage

--- v2/js/main.js
+++ v2/js/main.js
@@ -1,10 +1,10 @@
 "use strict";
 
 const setSectionsStylesForScrolling = () => {
-  $('section').each(function(index) {
-    $(this).css('z-index', index + 1);
-    $(this).css('top', `${index * 8}px`);
-    $(this).height(`calc(100vh - ${index * 8}px)`)
+  $("section").each(function (index) {
+    $(this).css("z-index", index + 1);
+    $(this).css("top", `${index * 8}px`);
+    $(this).height(`calc(100vh - ${index * 8}px)`);
   });
 };
 
@@ -46,12 +46,13 @@ function setScreenIndicatorAccordingToScroll() {
   const scrollTop = $(window).scrollTop() + 4;
   const sections = $("section");
   const windowHeight = $(window).height();
-  
+
   for (let i = 0; i < sections.length; i++) {
     const currentTop = $(sections[i]).offset().top - windowHeight / 2;
-    const nextTop = i + 1 < sections.length
-      ? $(sections[i + 1]).offset().top - windowHeight / 2
-      : Infinity;
+    const nextTop =
+      i + 1 < sections.length
+        ? $(sections[i + 1]).offset().top - windowHeight / 2
+        : Infinity;
 
     if (scrollTop >= currentTop && scrollTop < nextTop) {
       setScreenIndicator(i);
@@ -61,10 +62,11 @@ function setScreenIndicatorAccordingToScroll() {
 }
 
 $(document).ready(function () {
+  readSavedSettings();
+
   setSectionsStylesForScrolling();
-  $("body").attr("data-theme","light");
   window.scrollTo(0, 0);
-  
+
   $(".hero-text .message").each(function (i) {
     var $this = $(this);
     if (i >= 3) {
@@ -98,17 +100,26 @@ $(document).ready(function () {
         $(".navigation").css("box-shadow", "none");
       }, 500);
     }
-  $('.content').css('overflow', '');
+    $(".content").css("overflow", "");
   });
 
   $(".settings-container .close").click(function () {
-    if (document.querySelector(".settings-container").style.marginLeft !== "150%") {
-      $(".settings-container").css("margin-left", '150%' );
+    if (
+      document.querySelector(".settings-container").style.marginLeft !== "150%"
+    ) {
+      $(".settings-container").css("margin-left", "150%");
       setTimeout(() => {
         console.log("close settings");
         $(".navigation > *").css("filter", "");
         $(".settings-container").css("box-shadow", "none");
       }, 500);
+    }
+
+    const saveSettingsLocally = document.querySelector("#saveSettingsLocally");
+    if (saveSettingsLocally.checked) {
+      localStorage.setItem("theme", "light");
+    } else {
+      console.log("Settings will not be saved locally.");
     }
   });
 
@@ -126,7 +137,7 @@ $(document).ready(function () {
     $(".settings-container").css("margin-left", "calc(100% + 8px)");
     $(".navigation > :not(.settings-container)").css("filter", "blur(8px)");
     $(".settings-container").css("box-shadow", "var(--menu-box-shadow)");
-    $(".content").css('overflow', 'hidden');
+    $(".content").css("overflow", "hidden");
   });
 
   const messages = [];
@@ -155,40 +166,90 @@ $(document).ready(function () {
 
   $(".links a").click(function (e) {
     e.preventDefault();
-    $('.content').css('overflow', 'hidden');
-  const targetId = $(this).attr('href');
-  const target = $(targetId);
-    $('html, body').animate({
-      scrollTop: target.offset().top
-    }, 600); // 600 =
+    $(".content").css("overflow", "hidden");
+    const targetId = $(this).attr("href");
+    const target = $(targetId);
+    $("html, body").animate(
+      {
+        scrollTop: target.offset().top,
+      },
+      600
+    ); // 600 =
     $(".navigation .close").click();
-
   });
 });
 
 $(document).on("wheel", function (e) {
-  $('.content').css('overflow', '');
+  $(".content").css("overflow", "");
 });
 
-$('.copy').on('click', function (e) {
+$(".copy").on("click", function (e) {
   e.preventDefault();
-  const email = $('.email h5').text().trim();
-  navigator.clipboard.writeText(email)
-    .then(() => {
-      const $tooltip = $(this).find('.tooltip');
-      $tooltip.css('opacity', 1);
-      
-      setTimeout(() => {
-        $tooltip.css('opacity', 0);
-      }, 1500);
-    });
+  const email = $(".email h5").text().trim();
+  navigator.clipboard.writeText(email).then(() => {
+    const $tooltip = $(this).find(".tooltip");
+    $tooltip.css("opacity", 1);
+
+    setTimeout(() => {
+      $tooltip.css("opacity", 0);
+    }, 1500);
+  });
 });
 
-const darkModeToggle = document.querySelector('#darkModeSwitch');
-darkModeToggle.addEventListener('change', function () {
-  if (this.checked) {
-    $('body').attr('data-theme', 'dark');
-  } else {
-    $('body').attr('data-theme', 'light');
+function readSavedSettings(){
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme) {
+    $("body").attr("data-theme", savedTheme);
+    document.querySelector("#darkModeSwitch").checked = savedTheme === "dark";
+    document.querySelector("#saveSettingsLocally").checked = true;
   }
+
+  const showUnderGraduate = localStorage.getItem("underGraduate");
+  if(showUnderGraduate === "true"){
+    document.querySelector("#showUnderGraduateExperience").checked = true;
+    $(".undergraduate").show();
+  }
+}
+
+var darkModeToggle = document.querySelector("#darkModeSwitch");
+darkModeToggle.addEventListener("change", function () {
+  if(saveSettingsCheckbox.checked){
+    localStorage.setItem("theme", this.checked ? "dark" : "light");
+  }
+
+  console.log("Dark mode toggled:", localStorage.getItem("theme"));
+
+  if (this.checked) {
+    $("body").attr("data-theme", "dark");
+
+  } else {
+    $("body").attr("data-theme", "light");
+  }
+});
+
+var saveSettingsCheckbox = document.querySelector("#saveSettingsLocally");
+saveSettingsCheckbox.addEventListener("change", function () {
+  if (this.checked) {
+    localStorage.setItem("theme", $("body").attr("data-theme"));
+  } else {
+    localStorage.removeItem("theme");
+  }
+});
+
+const showUnderGraduateExperienceToggle = document.querySelector("#showUnderGraduateExperience");
+
+showUnderGraduateExperienceToggle.addEventListener("change", function () {
+  if(saveSettingsCheckbox.checked){
+    localStorage.setItem("underGraduate", this.checked);
+  }
+  // if (this.checked) {
+  //   console.log("Showing undergraduate experience");
+  //   $(".undergraduate").show();
+  //   document.querySelector(".undergraduate").show();
+  // } else {
+  //   console.log("Hiding undergraduate experience");
+  //   $(".undergraduate").hide();
+  //   document.querySelector(".undergraduate").hide();
+
+  // }
 });
