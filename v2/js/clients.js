a53aec8459caea15b54faf0a8dc8b16ad2454543
yasser-dlewati <yasdle@outlook.com>
2025-10-04 16:44:10 +0800

    Added more undergraduate experience

--- v2/js/clients.js
+++ v2/js/clients.js
@@ -31,71 +31,34 @@ function setExperienceDuration() {
   }
 }
 
-// function setScrollRightButtonVisibility() {
-//   scrollRightButton.style.display = console.log(
-//     divExperience.scrollLeft > maxDivExperienceWidth - 1
-//   );
-//   divExperience.scrollLeft > maxDivExperienceWidth - 1 ? "none" : "inline";
-// }
-
-// function setScrollLeftButtonVisibility() {
-//   scrollLeftButton.style.display = console.log(divExperience.scrollLeft === 0);
-//   divExperience.scrollLeft === 0 ? "none" : "inline";
-// }
-
-// divExperience.addEventListener("scroll", () => {
-//   setScrollRightButtonVisibility();
-//   setScrollLeftButtonVisibility();
-//   scrollPosition = divExperience.scrollLeft;
-// });
-
-// scrollLeftButton.addEventListener("click", function () {
-//   console.log(scrollPosition);
-//   if (scrollPosition > 0) {
-//     scrollPosition -= document.querySelector(".experience-item").clientWidth;
-//   }
-
-//   divExperience.scrollTo({
-//     left: scrollPosition,
-//     behavior: "smooth",
-//   });
-
-//   moveExperienceIndicatorByStep(-1);
-//   const isFirstIndicatorItemActive =
-//     document.querySelector(".experience-indicator li.active") ===
-//     document.querySelector(".experience-indicator li");
-//   if (isFirstIndicatorItemActive) {
-//     divExperience.scrollTo({
-//       left: 0,
-//       behavior: "smooth",
-//     });
-//   }
-// });
-
-// scrollRightButton.addEventListener("click", function () {
-//   console.log(maxDivExperienceWidth);
-//   if (scrollPosition < maxDivExperienceWidth) {
-//     scrollPosition += document.querySelector(".experience-item").clientWidth;
-//   }
-
-//   divExperience.scrollTo({
-//     left: scrollPosition,
-//     behavior: "smooth",
-//   });
-
-//   moveExperienceIndicatorByStep(1);
-//   const isLastIndicatorItemActive =
-//     document.querySelector(".experience-indicator li:last-child.active") ===
-//     document.querySelector(".experience-indicator li:last-child");
-//   if (isLastIndicatorItemActive) {
-//     divExperience.scrollTo({
-//       left: maxDivExperienceWidth,
-//       behavior: "smooth",
-//     });
-//   }
-// });
-
 const experienceJson = [
+  {
+    id: -3,
+    company: "Jotun Paints",
+    position: "Sales Executive",
+    duration: 18,
+    isCurrentCompany: false,
+    description:
+      `My first job ever. A place to learn how to deal with people, how to manage time and how to be responsible.\n 
+      My main responsibility was to help the clients, introduce the products and close deals.\n`,
+    webkitURL: "https://www.jotun.com/ww-en",
+    imageSrc: "jotun2.jpeg",
+    embededMapLink:"pb=!1m21!1m12!1m3!1d831.3169739645502!2d36.21109946875679!3d33.546415270793496!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e6!4m0!4m3!3m2!1d33.54617384120685!2d36.2111236086386!5e0!3m2!1sen!2smy!4v1759566409228!5m2!1sen!2smy"
+  },
+  {
+    id: -2,
+    company: "KidZone",
+    position: "Mentor",
+    duration: 24,
+    isCurrentCompany: false,
+    description:
+      `As \"Little Inventors\" program mentor, I was responsible for teaching kids the basics of physics, electricity, programming and robotics using age appropriate tools and methods to make sure they understand the concepts and have fun at the same time.\n
+      Also, I was responsible for organizing the annual events for the kids, playing roles in the plays that Kidzone hosted for the kids and their families.\n
+      Lastly, I was responsible for documenting the experiments and working on a simple syllabus to be followed by the mentors.`,
+    webkitURL: "https://www.facebook.com/KidZoneSyria/",
+    imageSrc: "Kidzone.jpeg",
+    embededMapLink:"pb=!1m18!1m12!1m3!1d318.486929755816!2d36.23401289362759!3d33.532001744626825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1518ddd8e68c44b9%3A0x2b47005f20b74050!2sSevan%20Restaurant!5e0!3m2!1sen!2smy!4v1759566571936!5m2!1sen!2smy"
+  },
   {
     id: -1,
     company: "Elaf Private School",
@@ -103,7 +66,7 @@ const experienceJson = [
     duration: 6,
     isCurrentCompany: false,
     description:
-      "My first job ever. A place to learn how to deal with people, how to manage time and how to be responsible.",
+      "I played the role of the link between the students, their families and the school. I was responsible for helping the students with resolving the diffeculties, guide them to the right path, in addition to organizing events and workshops to help the students to improve their skills.",
     webkitURL: "http://lms.elaf-school.com/",
     imageSrc: "elaf.png",
     embededMapLink:
@@ -131,7 +94,7 @@ const experienceJson = [
     description:
       "A place where I graded from a junior level to a senior. Learning a lot of the development cycle, collaporate with other departments to learn more, working hard to proof myself professionally and stepping up to be a team leader to keep the wheel rooling.",
     websiteUrl: "https://www.menumiz.com",
-    imageSrc: "menumiz.png",
+    imageSrc: "Menumiz.png",
     embededMapLink:
       "pb=!1m18!1m12!1m3!1d3983.9404660358446!2d101.66315911147416!3d3.1104541968520425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc48f196c74b49%3A0xa754e103215446f9!2sMenumiz%20Sdn.%20Bhd.!5e0!3m2!1sen!2smy!4v1756546615873!5m2!1sen!2smy",
   },
@@ -144,7 +107,7 @@ const experienceJson = [
     description:
       "Being the principle software engineer is something very hard but very rewarding at the same time. During this period of time, I got the chance to meet the brightest minds and not only work with, but learn from as well.",
     websiteUrl: "https://touchless.asia",
-    imageSrc: "touchless.png",
+    imageSrc: "Touchless.png",
     embededMapLink:
       "pb=!1m18!1m12!1m3!1d3983.7088143460246!2d101.66423242602517!3d3.171170203014705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc485de491c479%3A0xa40997f3dc918ec8!2sSolaris%20Dutamas%2C%201%2C%20Jln%20Dutamas%201%2C%20Solaris%20Dutamas%2C%2050754%20Kuala%20Lumpur%2C%20Wilayah%20Persekutuan%20Kuala%20Lumpur!5e0!3m2!1sen!2smy!4v1756546841444!5m2!1sen!2smy",
   },
