a1aa941a6639e6d1c24ba2b32db2d6a991b1051b
yasser-dlewati <yasdle@outlook.com>
2025-10-12 15:49:45 +0800

    Fixed bg color of services headers

--- v2/js/services.js
+++ v2/js/services.js
@@ -7,12 +7,13 @@ $('.service').click( function() {
         let isTargetDescriptionOpen = targetDescription.hasClass('show');
         if (isTargetDescriptionOpen) {
             $(this).find('i').removeClass('fa-square-plus').addClass('fa-square-minus');
-            //$(this).css('border-bottom', 'none');
             $(this).css('border-bottom', 'none');
             $('.service').not(this).css('border-bottom', '1px solid #ccc');
         } else {
             $(this).find('i').removeClass('fa-square-minus').addClass('fa-square-plus');
             $(this).css('border-bottom', '1px solid #ccc');
+            $(this).css('background-color', 'var(--bg-light-color)')
+            $(this).css('color', 'var(--text-dark-color)')
         }
     }, 400);
 });
